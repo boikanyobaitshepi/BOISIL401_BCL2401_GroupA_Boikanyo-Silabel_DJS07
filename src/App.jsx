28c109156c5d363d00ed22a8dc961441bae5dfc2
boikanyobaitshepi <156679487+boikanyobaitshepi@users.noreply.github.com>
2024-05-30 15:04:12 +0200

    export default

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
-function App() {
-
-  return (
-    <>
-     
-    </>
-  )
+import React from "react"
+ import Header from "./components/Header.jsx"
+ import Meme from "./components/Meme.jsx" 
+ 
+ 
+ 
+ 
+ export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
 }
 
-export default App
---
+++ src/component/Meme.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+export default function Meme() {
+    return (
+        <main>
+            <form className="form">
+            <div>
+                <input
+                    type="text" 
+                    placeholder="Shut up"
+                    className="form--input"
+                />
+             </div>
+             <div>
+                <input 
+                    type="text"
+                    placeholder="and take my money"
+                    className="form--input"
+                    />
+              </div>      
+                <button 
+                     className="form--button"
+                >
+                Get a new meme image 🖼
+                </button>
+            </form>
+        </main>
+    )
+}
